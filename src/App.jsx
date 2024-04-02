90816254e9fc0f02611ba22ebb99582a922a3577
Axel <meister.axel@gmail.com>
2024-04-02 14:22:42 +0200

    saved location name from API for display

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./style.css";
 
 function App() {
-  const [city, setCity] = React.useState("");
-  const [weather, setWeather] = React.useState(null);
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState(null);
+  const [locationName, setLocationName] = useState("");
 
   const APIkey = "f8182c0d84ffb542cf45f3485a0cc6de";
   const limit = "1";
@@ -19,9 +20,11 @@ function App() {
           if (!geocodingResponse.ok) {
             throw new Error("Failed to fetch location");
           }
-          const location = await geocodingResponse.json();
+          const locationData = await geocodingResponse.json();
+          const location = locationData[0];
+          setLocationName(location.name);
 
-          const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${location[0].lat}&lon=${location[0].lon}&units=${units}&exclude=${part}&appid=${APIkey}`;
+          const weatherUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${locationData[0].lat}&lon=${locationData[0].lon}&units=${units}&exclude=${part}&appid=${APIkey}`;
           const weatherResponse = await fetch(weatherUrl);
           if (!weatherResponse.ok) {
             throw new Error("Failed to fetch weather");
@@ -37,6 +40,12 @@ function App() {
     }
   }, [city]);
 
+  const keyPress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   const handleSearch = () => {
     setCity(document.getElementById("city").value);
   };
@@ -46,8 +55,14 @@ function App() {
       <div className="app-container">
         <h1>Weather App</h1>
         <p>This is a simple weather app using the OpenWeather API.</p>
-        <input type="text" placeholder="Berlin" id="city" />
+        <input
+          type="text"
+          placeholder="Berlin"
+          id="city"
+          onKeyDown={keyPress}
+        />
         <button onClick={handleSearch}>Search</button>
+        <h2>{locationName}</h2>
         <p>{weather && JSON.stringify(weather.current.temp)}</p>
         <p></p>
       </div>
